aa6066bc56128e46cf48561ff55674f6d3603ac4
Dachi <datschigordiashvili@gmail.com>
2022-10-21 14:06:40 +0400

    javascript challenge #3

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
-for (let i=1;i<=20;i++){
-    if (i%3 == 0 && i%5==0) {
-        console.log("Frontend Simplified")
+let strongo = "Frontend Simplified"
 
-    }
-    else if(i%5==0) {
-        console.log('Simplified')
-    }
-    else if(i%3==0) {
-        console.log("Frontend")
-    } 
-    else if (i%3 != 0 && i%5 != 0) {
-        console.log(i)
-    }
+for (let i = 0; i<=strongo.length - 1; i++){
+    console.log(`"${strongo[i]}"`)
 }
