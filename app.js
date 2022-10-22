5c6556d014dc8a912234a848f6ecc60664dfe27d
Dachi <datschigordiashvili@gmail.com>
2022-10-22 10:42:41 +0400

    objects

--- app.js
+++ app.js
@@ -1,9 +1,1 @@
-arr1 = ['A+','A','Fail']
-arr2 = ['Fail', 'Fail','B']
-
-function filterFails(array){
-    return array.filter(element => element != "Fail")
-}
-
-console.log(filterFails(arr1))
-console.log(filterFails(arr2))
+function register()
