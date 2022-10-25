b2ab411cf88c04323d374e34a10055a413fc10eb
Dachi <datschigordiashvili@gmail.com>
2022-10-25 11:58:56 +0400

    mediumos

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-function arraySum(array){
-    let sum = 0
-    for(let i = 0; i<array.length;i++){
-        sum = sum+array[i]
-    }
-    return sum
+function returnBoolean(array){
+    
 }
-
-console.log(arraySum([1,2,3,4]))
